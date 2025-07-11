99628e405e333bd096c31c646b545d9cc36a2a3c
koam@maticui <koam@maticui.com>
2025-07-11 12:20:45 +0530

    server index updated for cors 2

--- server/index.ts
+++ server/index.ts
@@ -43,10 +43,10 @@ app.use(passport.initialize())
 
 app.use((req, res, next) => {
     // res.setHeader("Access-Control-Allow-Origin", "https://kedar-client-fplsxt9pv-17komalkshirsagars-projects.vercel.app");
-    res.setHeader("Access-Control-Allow-Origin", "https://kedar-client.vercel.app/");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    // res.setHeader("Access-Control-Allow-Origin", "https://kedar-client.vercel.app/");
+    // res.setHeader("Access-Control-Allow-Credentials", "true");
+    // res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    // res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next();
 });
 
