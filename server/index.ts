1afec3d920fb529ad36df78e3cda9b0d819bce9a
koam@maticui <koam@maticui.com>
2025-07-11 12:12:35 +0530

    cors url change

--- server/index.ts
+++ server/index.ts
@@ -42,7 +42,8 @@ app.use(cors({
 app.use(passport.initialize())
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://kedar-client-fplsxt9pv-17komalkshirsagars-projects.vercel.app");
+    // res.setHeader("Access-Control-Allow-Origin", "https://kedar-client-fplsxt9pv-17komalkshirsagars-projects.vercel.app");
+    res.setHeader("Access-Control-Allow-Origin", "https://kedar-client.vercel.app/");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
